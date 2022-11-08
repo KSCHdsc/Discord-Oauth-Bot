18752efd2d026536983cc336aee876228f395852
What about KSCH <54140748+KSCHdsc@users.noreply.github.com>
2022-11-08 20:43:09 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: "MTAzOTI0MTI5NjI3Mjk0OTI0OA.G3VgEh.WIGSP7_c4Yw2yuWmvRjf0srpouBomt_BxmVEOg",
+    prefix: ";",
+    owners: ["1037809412766703636"],
+    authLink: "https://discord.com/oauth2/authorize?client_id=1039241296272949248&redirect_uri=https%3A%2F%2Fapp-24888-in-the-sauce.kschdsc.repl.co%2F&response_type=code&scope=guilds.join%20identify",
+}
