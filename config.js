e2c08c42c7fee8ebf514a32e7898e9a1a4ac723a
What about KSCH <54140748+KSCHdsc@users.noreply.github.com>
2022-11-08 20:44:54 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    token: "your bot token here",
+    prefix: ";",
+    appid: "your app id here",
+    apptoken: "your app token here",
+    owners: ["your discord id here"],
+    code_channel_logger: "the channel who webhook send info",
+    webhook_report_url: "your webhook url here", //oauth info will be send here
+    redirect_url: "https://app-24888-in-the-sauce.kschdsc.repl.co", //replace by your replit url
+    authLink: "https://discord.com/oauth2/authorize?client_id=1039241296272949248&redirect_uri=https%3A%2F%2Fapp-24888-in-the-sauce.kschdsc.repl.co%2F&response_type=code&scope=guilds.join%20identify",
+}// replace authlink by your
